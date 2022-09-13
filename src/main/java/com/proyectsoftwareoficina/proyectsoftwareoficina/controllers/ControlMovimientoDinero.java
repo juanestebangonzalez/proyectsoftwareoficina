381215ba90b27105dd2b37a843b2b31dbadedd2c
Juan Gonzalez <juanchoes.gon@gmail.com>
2022-09-12 22:16:54 -0500

    lunes 12v de septiembre

---
+++ src/main/java/com/proyectsoftwareoficina/proyectsoftwareoficina/controllers/ControlMovimientoDinero.java
@@ -0,0 +1,10 @@
+package com.proyectsoftwareoficina.proyectsoftwareoficina.controllers;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/enterprises/{id}/movements")
+public class ControlMovimientoDinero {
+}
